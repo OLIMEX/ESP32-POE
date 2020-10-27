ee1633818c31c0eefd8163c65c2e941bad68c516
DanKoloff <lubchoh@yahoo.com>
2020-10-27 10:31:59 +0200

    New example
    Ethernet + WIFI Arduino demo

---
+++ SOFTWARE/ARDUINO/ESP32_PoE_Ethernet_WIFI_test/ESP32_PoE_Ethernet_WIFI_test.ino
@@ -0,0 +1,177 @@
+/*
+ * Simple demo to demonstrate functionality of Ethernet and WiFi simultaneously 
+ * If successful upon initializatoin on the terminal will be shown the IP adresses
+ * of each connection. The output will be something like this:
+ * ---------------------------------------------------------------------------------
+ * Connecting to <SSID>
+ * ETH Started
+ * ....
+ * WiFi connected
+ * IP address: 
+ * 192.168.0.204
+ * ETH Connected
+ * ETH MAC: 3C:71:BF:0D:89:5B, IPv4: 192.168.0.152, FULL_DUPLEX, 100Mbps
+ * ---------------------------------------------------------------------------------
+ * every 3 seconds:
+ * 1) ethernet tries to connect to google.com
+ * 2) WiFi tries to connect to data.sparkfun.com
+ * For the WiFi you should edit the variables
+ * "ssid" and "password" with the ones of your access point.
+ */
+
+
+#include <ETH.h>
+
+static bool eth_connected = false;
+
+void WiFiEvent(WiFiEvent_t event)
+{
+  switch (event) {
+    case SYSTEM_EVENT_ETH_START:
+      Serial.println("ETH Started");
+      //set eth hostname here
+      ETH.setHostname("esp32-ethernet");
+      break;
+    case SYSTEM_EVENT_ETH_CONNECTED:
+      Serial.println("ETH Connected");
+      break;
+    case SYSTEM_EVENT_ETH_GOT_IP:
+      Serial.print("ETH MAC: ");
+      Serial.print(ETH.macAddress());
+      Serial.print(", IPv4: ");
+      Serial.print(ETH.localIP());
+      if (ETH.fullDuplex()) {
+        Serial.print(", FULL_DUPLEX");
+      }
+      Serial.print(", ");
+      Serial.print(ETH.linkSpeed());
+      Serial.println("Mbps");
+      eth_connected = true;
+      break;
+    case SYSTEM_EVENT_ETH_DISCONNECTED:
+      Serial.println("ETH Disconnected");
+      eth_connected = false;
+      break;
+    case SYSTEM_EVENT_ETH_STOP:
+      Serial.println("ETH Stopped");
+      eth_connected = false;
+      break;
+    default:
+      break;
+  }
+}
+
+void testClient(const char * host, uint16_t port)
+{
+  Serial.print("\nconnecting to ");
+  Serial.println(host);
+
+  WiFiClient client;
+  if (!client.connect(host, port)) {
+    Serial.println("connection failed");
+    return;
+  }
+  client.printf("GET / HTTP/1.1\r\nHost: %s\r\n\r\n", host);
+  while (client.connected() && !client.available());
+  while (client.available()) {
+    Serial.write(client.read());
+  }
+
+  Serial.println("closing connection\n");
+  client.stop();
+}
+
+#include <WiFi.h>
+
+const char* ssid     = "SSID";
+const char* password = "PASSWORD";
+
+const char* host = "data.sparkfun.com";
+const char* streamId   = "....................";
+const char* privateKey = "....................";
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(10);
+
+    // ETH
+    Serial.begin(115200);
+    WiFi.onEvent(WiFiEvent);
+    ETH.begin();
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+int value = 0;
+
+void loop()
+{
+  if (eth_connected) {
+    testClient("google.com", 80);
+  }
+    delay(3000);
+    ++value;
+
+    Serial.print("connecting to ");
+    Serial.println(host);
+
+    // Use WiFiClient class to create TCP connections
+    WiFiClient client;
+    const int httpPort = 80;
+    if (!client.connect(host, httpPort)) {
+        Serial.println("connection failed");
+        return;
+    }
+
+    // We now create a URI for the request
+    String url = "/input/";
+    url += streamId;
+    url += "?private_key=";
+    url += privateKey;
+    url += "&value=";
+    url += value;
+
+    Serial.print("Requesting URL: ");
+    Serial.println(url);
+
+    // This will send the request to the server
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n\r\n");
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+        if (millis() - timeout > 5000) {
+            Serial.println(">>> Client Timeout !");
+            client.stop();
+            return;
+        }
+    }
+
+    // Read all the lines of the reply from server and print them to Serial
+    while(client.available()) {
+        String line = client.readStringUntil('\r');
+        Serial.print(line);
+    }
+
+    Serial.println();
+    Serial.println("closing connection");
+    delay(3000);
+}
