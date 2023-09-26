03d45ccf1edfdff7b0df9410541815f1ebb5a4f8
Kevin Kadow <99362831+Kv603@users.noreply.github.com>
2023-09-26 14:21:56 -0400

    Update ESP32_PoE_Ethernet_Arduino.ino
    Additional debug output

--- SOFTWARE/ARDUINO/ESP32_PoE_Ethernet_Arduino/ESP32_PoE_Ethernet_Arduino.ino
+++ SOFTWARE/ARDUINO/ESP32_PoE_Ethernet_Arduino/ESP32_PoE_Ethernet_Arduino.ino
@@ -6,22 +6,29 @@
 #define ETH_PHY_POWER 12
 
 #include <ETH.h>
-
+#define TESTHOSTNAME "google.com"
 
 static bool eth_connected = false;
 
-void WiFiEvent(WiFiEvent_t event)
-{
+void WiFiEvent(WiFiEvent_t event) {
   switch (event) {
     case SYSTEM_EVENT_ETH_START:
-      Serial.println("ETH Started");
+    case 20:  //ESP32 ethernet start
+      Serial.print("ETH Started ");
+      Serial.println((int)event);
       //set eth hostname here
       ETH.setHostname("esp32-ethernet");
       break;
+
     case SYSTEM_EVENT_ETH_CONNECTED:
-      Serial.println("ETH Connected");
+    case 22:  //ESP32 ethernet phy link up
+      Serial.print("ETH Connected ");
+      Serial.println((int)event);
       break;
     case SYSTEM_EVENT_ETH_GOT_IP:
+    case 24:  //ESP32 ethernet got IP
+      Serial.print("ETH Got an IP address ");
+      Serial.println((int)event);
       Serial.print("ETH MAC: ");
       Serial.print(ETH.macAddress());
       Serial.print(", IPv4: ");
@@ -35,20 +42,24 @@ void WiFiEvent(WiFiEvent_t event)
       eth_connected = true;
       break;
     case SYSTEM_EVENT_ETH_DISCONNECTED:
+    case 23:  // ESP32 ethernet phy link down
       Serial.println("ETH Disconnected");
       eth_connected = false;
       break;
     case SYSTEM_EVENT_ETH_STOP:
+    case 21:  //ESP32 ethernet stop
       Serial.println("ETH Stopped");
       eth_connected = false;
       break;
+
     default:
+      Serial.print("Unknown network event ");
+      Serial.println((int)event);
       break;
   }
 }
 
-void testClient(const char * host, uint16_t port)
-{
+void testClient(const char* host, uint16_t port) {
   Serial.print("\nconnecting to ");
   Serial.println(host);
 
@@ -58,7 +69,8 @@ void testClient(const char * host, uint16_t port)
     return;
   }
   client.printf("GET / HTTP/1.1\r\nHost: %s\r\n\r\n", host);
-  while (client.connected() && !client.available());
+  while (client.connected() && !client.available())
+    ;
   while (client.available()) {
     Serial.write(client.read());
   }
@@ -67,18 +79,26 @@ void testClient(const char * host, uint16_t port)
   client.stop();
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
+  Serial.println("Boot");
+  delay(200);
+  Serial.println("Starting ETH");
   WiFi.onEvent(WiFiEvent);
   ETH.begin();
+  delay(200);
+  Serial.print("ETH MAC: ");
+  Serial.println(ETH.macAddress());
 }
 
-
-void loop()
-{
+void loop() {
+  Serial.print(", IPv4: ");
+  Serial.println(ETH.localIP());
   if (eth_connected) {
-    testClient("google.com", 80);
+    testClient(TESTHOSTNAME, 80);
+    Serial.print("Sleeping...");
+    delay(50000);
+    Serial.println(".");
   }
-  delay(10000);
+  delay(1000);
 }
