937317bafbefa63cf7ba469ced46fa6f45222478
Francois Planque <github@fplanque.net>
2024-03-03 21:08:08 +0100

    Added comments for beginners
    (sketch behaviour remains the same)

--- SOFTWARE/ARDUINO/ESP32_PoE_Ethernet_Arduino/ESP32_PoE_Ethernet_Arduino.ino
+++ SOFTWARE/ARDUINO/ESP32_PoE_Ethernet_Arduino/ESP32_PoE_Ethernet_Arduino.ino
@@ -1,26 +1,33 @@
 /*
-    This sketch shows the Ethernet event usage
-
+  This sketch will log Ethernet events to the serial console
+  It will also log the MAC address and current IP address every second (IP can be assigned by DHCP)
 */
 
 #include <ETH.h>
 
-
 static bool eth_connected = false;
 
+
+// React to Ethernet events:
 void WiFiEvent(WiFiEvent_t event)
 {
   switch (event) {
+
     case ARDUINO_EVENT_ETH_START:
+      // This will happen during setup, when the Ethernet service starts
       Serial.println("ETH Started");
       //set eth hostname here
       ETH.setHostname("esp32-ethernet");
       break;
+
     case ARDUINO_EVENT_ETH_CONNECTED:
+      // This will happen when the Ethernet cable is plugged 
       Serial.println("ETH Connected");
       break;
+
     case ARDUINO_EVENT_ETH_GOT_IP:
-      Serial.print("ETH MAC: ");
+    // This will happen when we obtain an IP address through DHCP:
+      Serial.print("Got an IP Address for ETH MAC: ");
       Serial.print(ETH.macAddress());
       Serial.print(", IPv4: ");
       Serial.print(ETH.localIP());
@@ -31,24 +38,36 @@ void WiFiEvent(WiFiEvent_t event)
       Serial.print(ETH.linkSpeed());
       Serial.println("Mbps");
       eth_connected = true;
+
+      // Uncomment to automatically make a test connection to a server:
+      // testClient( "192.168.0.1", 80 );
+
       break;
+
     case ARDUINO_EVENT_ETH_DISCONNECTED:
+      // This will happen when the Ethernet cable is unplugged 
       Serial.println("ETH Disconnected");
       eth_connected = false;
       break;
+
     case ARDUINO_EVENT_ETH_STOP:
+      // This will happen when the ETH interface is stopped but this never happens
       Serial.println("ETH Stopped");
       eth_connected = false;
       break;
+
     default:
       break;
   }
 }
 
+// Try to read something from a webserver:
 void testClient(const char * host, uint16_t port)
 {
-  Serial.print("\nconnecting to ");
-  Serial.println(host);
+  Serial.print("\nConnecting to ");
+  Serial.print(host);
+  Serial.print(":");
+  Serial.println(port);
 
   WiFiClient client;
   if (!client.connect(host, port)) {
@@ -67,8 +86,17 @@ void testClient(const char * host, uint16_t port)
 
 void setup()
 {
-  Serial.begin(115200);
+  // This sketch will log Ethernet events to the serial console:
+  Serial.begin(115200); // Assuming computer will be connected to serial port at 115200 bauds
+  Serial.print("Setup...");
+  
+  // Add a handler for network events. This is misnamed "WiFi" because the ESP32 is historically WiFi only,
+  // but in our case, this will react to Ethernet events.
+  Serial.print("Registering event handler for ETH events...");
   WiFi.onEvent(WiFiEvent);
+  
+  // Starth Ethernet (this does NOT start WiFi at the same time)
+  Serial.print("Starting ETH interface...");
   ETH.begin();
 }
 
